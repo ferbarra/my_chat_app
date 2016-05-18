89e6b18897d99a9223af3b449d41e42024aa2a89
ferbarra2207 <fernandobarra2207@gmail.com>
2016-05-18 08:17:56 +0000

    Application now keeps the 10 most recent messages on a redis database.

--- server_files/app.js
+++ server_files/app.js
@@ -3,21 +3,55 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const redis = require('redis');
+const redisClient = redis.createClient();
 
-io.on('connection', function(client) {
+var storeMessage = function (name, data) {
+    var message = JSON.stringify({name: name, data: data});
     
-    client.on('join', function(name) {
-        client.nickname = name;
-        client.broadcast.emit('chat', `${name} joined the chat`);
+    redisClient.rpush('messages', message, function(error, reply) {
+        redisClient.ltrim('messages', 0, 9);
+    });
+};
+
+io.on('connection', function(socket) {
+    
+    socket.on('join', function(name) {
+        socket.nickname = name;
+        
+        socket.broadcast.emit('display users', name);
+        
+        redisClient.smembers('users', function(error, users) {
+            users.forEach(function(user) {
+                socket.emit('display users', user);
+            });
+        });
+        
+        redisClient.sadd('users', name);
+        
+        socket.broadcast.emit('chat', `${name} joined the chat`);
+        
+        redisClient.lrange("messages", 0, -1, function(error, messages) {
+            messages.forEach(function(message) {
+                message = JSON.parse(message);
+                socket.emit('messages', `${message.name}: ${message.data}`);
+            });
+        });
+        
     });
     
-    client.on('messages', function(data) {
+    socket.on('messages', function(data) {
         console.log(data);
         
-        var nickname = client.nickname;
+        var nickname = socket.nickname;
         
-        client.broadcast.emit('messages', `${nickname}: ${data}`);
-        client.emit('messages', `${nickname}: ${data}`);
+        socket.broadcast.emit('messages', `${nickname}: ${data}`);
+        socket.emit('messages', `${nickname}: ${data}`);
+        storeMessage(nickname, data);
+    });
+    
+    socket.on('disconnect', function(user) {
+        socket.broadcast.emit('remove user', user);
     });
     
 });
